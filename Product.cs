bd305214afc6241be15dc94d3e8adb0248cbbb6e
Daniel <dannynebs@gmail.com>
2024-12-12 19:55:58 -0500

    Completed InventoryManager and Product Classes

--- Product.cs
+++ Product.cs
@@ -8,5 +8,16 @@ namespace PointOfSaleTerminal
 {
     internal class Product
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public double Price {  get; set; }
+        public Product(string name, string description, string category, double price)
+        {
+            Name = name;
+            Description = description;
+            Category = category;
+            Price = price;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using PointOfSaleTerminal;
+
 Console.WriteLine("Hello, World!");
 
+InventoryManager newInventory = new InventoryManager();
+
+newInventory.AddProduct(new Product("Blah", "blah", "blah", 1.00));
+newInventory.DiplayProduct();
