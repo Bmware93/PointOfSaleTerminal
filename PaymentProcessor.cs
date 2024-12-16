425cb4e9d59c39d4a0166b51eebac607bca6a37b
Rheyya Hollins <hollinsrheyya@gmail.com>
2024-12-16 18:43:57 -0500

    initail

---
+++ PaymentProcessor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSaleTerminal
+{
+    public class PaymentProcessor
+    { 
+       //
+
+        public void CalculateTotalPrice(List < Product > items )
+        {
+            double subTotal = 0;
+            double salesTax = 0.06;
+            double grandTotal;
+
+
+            for ( int i = 0; i < items.Count; i++ )
+            {
+                subTotal += items[i].Price * items.quantity;
+            }
+            grandTotal = subTotal + subTotal * salesTax;
+        }
+
+    };
+
+    
+
+
+
+
+}
